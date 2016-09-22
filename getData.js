228a5b62f9b0219daf3f2f64f317d8ef320d1291
dianjie <994833478@qq.com>
2016-09-22 18:46:06 +0800

    添加获取数据函数文件
    添加获取数据函数文件

---
+++ getData.js
@@ -0,0 +1,32 @@
+var execReg=/<span lang="EN-US">(.*?)<span>&nbsp;&nbsp;&nbsp;&nbsp; <\/span><\/span><span style="font-family: 宋体">(.*?)<\/span>/;
+var resArr=[];
+var sign=null;
+for(var i=0,len=document.getElementsByClassName("MsoNormal").length;i<len;i++){
+  var data=document.getElementsByClassName("MsoNormal")[i].innerHTML;
+  var result=execReg.exec(data);
+  if(len==0){
+    sign=result[1].slice(0,2);
+    resArr.push({value: result[2].slice(1),label: result[2].slice(1)})
+  }else{
+    if(result[1].slice(0,2)!==sign){
+      sign=result[1].slice(0,2);
+      resArr.push({value: result[2].slice(1),label: result[2].slice(1)})
+    }else{
+      if(result[2].match(/\s/ig).length==2){
+          var arr=resArr[resArr.length-1].children||[];
+          arr.push({value: result[2].slice(2),label: result[2].slice(2)});
+          resArr[resArr.length-1].children=arr;
+      }else{
+          var arr1=resArr[resArr.length-1].children||[];
+          var l=arr1.length?arr1.length:1;
+          var arr=resArr[resArr.length-1].children[l-1].children||[];
+          arr.push({value: result[2].slice(3),label: result[2].slice(3)})
+          resArr[resArr.length-1].children[l-1].children=arr;
+          resArr=resArr
+
+      }
+
+    }
+
+}}
+console.log(JSON.stringify(resArr))
